13079619f27d3cd1bae5d4d7eeb76b03dc84d7da
masayasviel <33654883+masayasviel@users.noreply.github.com>
2024-01-07 14:01:03 +0900

    :wrench: env

---
+++ src/app/shared/constants/firebase.ts
@@ -0,0 +1,23 @@
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+import { environment } from '@/environments/environment';
+
+const firebaseConfig = {
+  apiKey: environment.apiKey,
+  authDomain: environment.authDomain,
+  projectId: environment.projectId,
+  storageBucket: environment.storageBucket,
+  messagingSenderId: environment.messagingSenderId,
+  appId: environment.appId,
+  measurementId: environment.measurementId,
+};
+
+export const firebaseApp  = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(firebaseApp);
+export const firestore = getFirestore(firebaseApp);
+export const firebaseStorage = getStorage(firebaseApp);
+export const firebaseAuth = getAuth(firebaseApp);
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  apiKey: process.env['NG_API_KEY'],
+  authDomain: process.env['NG_AUTH_DOMAIN'],
+  projectId: process.env['NG_PROJECT_ID'],
+  storageBucket: process.env['NG_STORAGE_BUCKET'],
+  messagingSenderId: process.env['NG_MESSAGING_SENDER_ID'],
+  appId: process.env['NG_APP_ID'],
+  measurementId: process.env['NG_MEASUREMENT_ID'],
+};
