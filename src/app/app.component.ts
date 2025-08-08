4e5ba5674cf84cec00930304ef399ce269056472
masayasviel <33654883+masayasviel@users.noreply.github.com>
2025-08-09 00:05:48 +0900

    :arrow_up:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastModule } from 'primeng/toast';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, ToastModule],
+    imports: [RouterOutlet, ToastModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
